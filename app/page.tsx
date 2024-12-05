19c19e023a0e1e7e512970e82c73f10bff96df31
bunz <bunz5911@gmail.com>
2024-12-05 16:19:43 +0900

    login: 로그인

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../../amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 Amplify.configure(outputs);
 
@@ -14,6 +15,7 @@ const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const { user } = useAuthenticator();
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
@@ -31,14 +33,13 @@ export default function App() {
     });
   }
 
-    
   function deleteTodo(id: string) {
     client.models.Todo.delete({ id })
   }
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId ? `${user.signInDetails.loginId}'s todos` : 'My todos'}</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
